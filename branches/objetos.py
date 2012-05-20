b7371b24f0dc80122430dbd3f96711cb55fadaa5
jupmorenor@gmail.com <jupmorenor@gmail.com@870897f8-f528-03ff-81b3-6bcd53e681a0>
2012-05-20 18:51:51 +0000

    Se hace la clase instanciable para poder usar los atributos definidos en el constructor desde la interfaz en la que sea instanciada. Se agrega un metodo cargar_musica (linea 42) diferente de cargar_sonido ya que el primero usa pygame.mixer.music y el segundo pygame.mixer.Sound que gestionan los archivos de audio de forma diferente.

--- branches/objetos.py
+++ branches/objetos.py
@@ -11,111 +11,74 @@
 Created on 10/05/2012
 @author: Juan Pablo Moreno y Alejandro Duarte
 '''
-import pygame, sys, random
-from pygame.locals import *
-from generales import *
-# ----------------------------------------------
-# Clases
-# ----------------------------------------------
-class Base_de_Tanque(pygame.sprite.Sprite):
-	"""Objeto tanque del primer nivel"""
+
+import pygame
+from pygame.locals import*
+from math import degrees, radians, sin, cos, atan2
+
+class Generales():
+
+	"""Constantes para uso general de cualquier objeto"""
 	def __init__(self):
-		pygame.sprite.Sprite.__init__(self)
-		self.preimagen = Generales.cargar_imagen("ruta")#por definir
-		self.postimagen = self.preimagen
-		self.rectangulo = self.preimagen.get_rect()
-		self.rectangulo.center = [400, 400]
-		self.velocidad = 5
-		
-	def actualizar(self, evento):
-		if evento.key==K_ESCAPE:
-			pygame.quit()
-			sys.exit(0)
-		if evento.key==K_UP:
-			self.rectangulo.centery-=self.velocidad
-			self.postimagen=pygame.transform.rotate(self.preimagen,90)
-		if evento.key==K_DOWN:
-			self.rectangulo.centery+=self.velocidad
-			self.postimagen=pygame.transform.rotate(self.preimagen,270)
-		if evento.key==K_LEFT:
-			self.rectangulo.centerx-=self.velocidad
-			self.postimagen=pygame.transform.rotate(self.preimagen,180)
-		if evento.key==K_RIGHT:
-			self.rectangulo.centerx+=self.velocidad
-			self.postimagen=pygame.transform.rotate(self.preimagen,0)
-		self.rectangulo = [min(max(self.rectangulo.centerx,-8),Generales.VENTANA[0]-32), min(max(self.rectangulo.centery,-8),Generales.VENTANA[1]-32)]
-		
-class Rotor_de_Tanque(pygame.sprite.Sprite):
-	"""Objeto tanque del primer nivel"""
-	def __init__(self, ruta_img, ruta_snd):
-		pygame.sprite.Sprite.__init__(self)
-		self.preimagen = Generales.cargar_imagen(ruta_img)
-		self.postimagen = self.preimagen
-		self.rectangulo = self.preimagen.get_rect()
-		self.disparo = Generales.cargar_sonido(ruta_snd)
-		self.balas_disparadas=[]
-		
-	def actualizar(self, mouse, ventana, baseTanque):
-		self.angulo = Generales.direccion_punto(self.rectangulo.centerx, self.rectangulo.centery, mouse)
-		self.postimagen = pygame.transform.rotate(self.preimagen, self.angulo)
-		self.rectangulo.center = baseTanque.rectangulo.center
-		ventana.blit(self.postimagen, self.rectangulo)
-		
-	def disparar(self, boton_mouse):
-		if boton_mouse:
-			bala = Bala(self.rectangulo, self.angulo)
-			self.disparo.play()
-			self.balas_disparadas.append(bala)
-		
-class Enemigo_Tanque(Rotor_de_Tanque):
-	"""Objeto enemigo del primer nivel"""
-	def __init__(self, ruta_img, ruta_snd):
-		pygame.sprite.Sprite.__init__(self)
-		self.preimagen = Generales.cargar_imagen(ruta_img)
-		self.postimagen = self.preimagen
-		self.rectangulo = self.preimagen.get_rect()
-		self.disparo = Generales.cargar_sonido(ruta_snd)
-		self.balas_disparadas=[]
-		self.velocidad = 2
+		self.VENTANA=self.ANCHO,self.ALTO=800,600
 	
-	def actualizar(self, direccion):
-		self.angulo = Generales.direccion_punto(self.rectangulo.centerx, self.rectangulo.centery, direccion)
-		self.postimagen = pygame.transform.rotate(self.preimagen, self.angulo)
-		self.rectangulo.centerx+=Generales.vector_en_x(self.velocidad, self.angulo)
-		self.rectangulo.centery+=Generales.vector_en_y(self.velocidad, self.angulo)
-      
-  def disparar(self):
-		bala = Bala(self.rectangulo, self.angulo)
-		self.balas_disparadas.append(bala)
-    #falta definir la forma en que el tanque enemigo dispara
-		pass
+	"""Funciones para uso general de cualquier objeto"""
+	def cargar_imagen(self, ruta):
+		"""funcion para cargar imagenes"""
+		try: imagen=pygame.image.load(ruta).convert_alpha()
+		except(pygame.error): #en caso de error
+			print("No se pudo cargar la imagen: ", ruta)
+			raise(SystemExit)
+		return imagen
+		
+	def cargar_sonido(self, ruta):
+		"""Funcion para cargar sonidos"""
+		try:sonido=pygame.mixer.Sound(ruta)
+		except(pygame.error):
+			print("No se pudo cargar el sonido: ", ruta)
+			sonido=None
+		return sonido
 	
-class Bala(pygame.sprite.Sprite):
-	"""Objeto bala general para todos los objetosq ue disparan"""
-	def __init__(self, ruta_img, posicion_inicial, angulo):
-		pygame.sprite.Sprite.__init__(self)
-		self.angulo = angulo
-		self.imagen = pygame.transform.rotate(Generales.cargar_imagen(ruta_img), self.angulo)
-		self.rectangulo = self.imagen.get_rect()
-		self.velocidad = 4
-		self.rectangulo.center = posicion_inicial
+	def cargar_musica(self, ruta):
+		"""Funcion para cargar musica"""
+		try: pygame.mixer.music.load(ruta)
+		except(pygame.error):
+			print("No se pudo cargar la cancion: ", ruta)
 		
-	def actualizar(self):
-		self.rectangulo.centerx+=Generales.vector_en_x(self.velocidad, self.angulo)
-		self.rectangulo.centery+=Generales.vector_en_y(self.velocidad, self.angulo)
-		if self.rectangulo.centerx > Generales.VENTANA[0] or self.rectangulo.centerx < 0 or self.rectangulo.centery > Generales.VENTANA[1] or self.rectangulo.centery < 0:
-			self.kill()
+	def texto(self, tipo_letra, texto, posx, posy, color):
+		"""Funcion para escritura de texto"""
+		fuente = pygame.font.Font(tipo_letra, 25)
+		salida = pygame.font.Font.render(fuente, texto, 1, color)
+		salida_rect = salida.get_rect()
+		salida_rect.centerx = posx
+		salida_rect.centery = posy
+		return salida, salida_rect
+	
+	def agregar_imagen(self, ruta, largo, alto):
+		"""Funcion para cortar las subimagenes de una imagen animada"""
+		subimagenes = []
+		imagen_completa = self.cargar_imagen(ruta)
+		ancho_total, altura_total = imagen_completa.get_size()
+		for i in range(int(ancho_total/largo)):
+			subimagenes.append(imagen_completa.subsurface((i*largo,0,largo,alto)))
+		return subimagenes
+	
+	def direccion_punto(self, x,y,x2_y2,):
+		"""Funcion que devuelve la direccion que hay hacia un punto"""
+		x2, y2 = x2_y2
+		dist_x = x2-x
+		dist_y = y2-y
+		direccion=-1*degrees(atan2(dist_y,dist_x))
+		return direccion
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	def vector_en_x(self, length, dirr):
+		"""Devuelve el componente x de un vector"""
+		x_dist=cos(radians(dirr)) * length
+		return x_dist
+	
+	def vector_en_y(self, length, dirr):
+		"""Devuelve el componente y de un vector"""
+		y_dist=sin(radians(dirr)) * -length
+		return y_dist
+
 		
