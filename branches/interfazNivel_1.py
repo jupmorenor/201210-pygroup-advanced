5238c298484da9acc0dc5c8c5f8e24e0fd8ba72d
jupmorenor@gmail.com <jupmorenor@gmail.com@870897f8-f528-03ff-81b3-6bcd53e681a0>
2012-06-02 13:35:41 +0000

    Ajustes de acuerdo a cambios en r24 sobre creacion y administracion de enemigos y apertura preliminar de la forma en que se destruyen. Falta agregar las colisiones.

--- branches/interfazNivel_1.py
+++ branches/interfazNivel_1.py
@@ -29,8 +29,35 @@ class Nivel1():
 		self.rotor_tanque = Rotor_de_Tanque("imagenes/nivel 1/tanque_rotor_Nv_1.png", "sonido/Explosion01.ogg")
 		self.pos_mouse = []
 		self.bot_mouse = 0
-		self.balas = []
+		self.enemigos = []
+		self.explosiones = []
+		self.alarma = 30
 		
+		#self.enemigo=Enemigo_Tanque("imagenes/nivel 1/tanque_enemigo_Nv_1.png", "sonido/Explosion01.ogg")
+		
+	def controlEnemigos(self):
+		self.alarma -= 0.1
+		if self.alarma<=0:
+			xrandom = random.randrange(0,int(funciones.VENTANA[0]))
+			yrandom = random.randrange(0,int(funciones.VENTANA[1]))
+			enemigo=Enemigo_Tanque(xrandom, yrandom, "imagenes/nivel 1/tanque_enemigo_Nv_1.png", "sonido/Explosion01.ogg")
+			self.enemigos.append(enemigo)
+			self.alarma=30
+		for i in range(len(self.enemigos)):
+			self.enemigos[i].actualizar(self.base_tanque.posicion)
+			self.enemigos[i].disparar()
+			self.ventana.blit(self.enemigos[i].postimagen, self.enemigos[i].rect)
+			#if self.enemigos[i].destruir():
+			#	sale=self.enemigos.pop(i)
+			#	explosion=Explosion(sale.rect.center)
+			#	self.explosiones.append(explosion)
+		
+	def controlExplosiones(self):
+		for i in range(len(self.explosiones)):
+			if self.explosiones[i].actualizar(self.ventana):
+				self.explosiones.pop(i)
+				break
+				
 	def mainNivel1(self):
 		pygame.key.set_repeat(1,25)
 		pygame.mixer.music.play(-1)
@@ -50,10 +77,15 @@ class Nivel1():
 				if evento.type == pygame.MOUSEBUTTONDOWN:
 					self.rotor_tanque.disparar(self.bot_mouse)
 		
+			#self.enemigo.actualizar(self.base_tanque.posicion)
+			#self.enemigo.disparar()	
+				
 			#Seccion de dibujo
 			self.ventana.blit(self.imagen_fondo, (0,0))
 			self.ventana.blit(self.base_tanque.postimagen, self.base_tanque.rect)
 			self.rotor_tanque.actualizar(self.pos_mouse, self.ventana, self.base_tanque.rect.center)
+			
+			self.controlEnemigos()
 			self.rotor_tanque.dibujar_Balas(self.ventana)
 			self.rotor_tanque.actualizar1(self.ventana)
 			pygame.display.update()
