cb82286c1ff65fbd0924b59f9277c456eba73946
alejandrod.92@gmail.com <alejandrod.92@gmail.com@870897f8-f528-03ff-81b3-6bcd53e681a0>
2012-06-17 03:16:55 +0000

    menu de la interfaz

---
+++ branches/menu.py
@@ -0,0 +1,71 @@
+
+
+
+
+import pygame
+import interfazNivel_1
+
+
+ # importo el modulo
+
+class Cursor(pygame.Rect):
+	def __init__(self):
+		pygame.Rect.__init__(self,0,0,1,1)
+	def update(self):
+		self.left,self.top=pygame.mouse.get_pos()
+class Boton(pygame.sprite.Sprite):
+	def __init__(self,imagen, x, y):
+		self.imagen1=imagen
+		self.rect= self.imagen1.get_rect()
+		self.rect.left,self.rect.top=(x,y)
+		
+#funcion main
+def principal():
+    pygame.init() 
+        
+    
+    
+    pantalla=pygame.display.set_mode((500,400))
+    
+    pygame.display.set_caption("Juego") 
+    #creo un reloj para controlar los fps
+    reloj1=pygame.time.Clock()
+    cursor1=Cursor()
+    
+    boton1= Boton(pygame.image.load("boton1.png"),200,100)#boton Interfaz nivel 1
+    boton2= Boton(pygame.image.load("boton2.png"),200,200)#boton Salir 
+    
+    blanco=(255,255,255) 
+    azul=(0,200,10)
+    colorF=blanco
+    
+    salir=False
+    #LOOP PRINCIPAL
+    while salir!=True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                salir=True
+            if event.type== pygame.MOUSEBUTTONDOWN:
+            	if cursor1.colliderect(boton1.rect):
+            		import interfazNivel_1
+            		nivel1=interfazNivel_1.Nivel1()
+            		nivel1.mainNivel1()
+            		salir=True
+            	else: colorF=blanco
+            	if cursor1.colliderect(boton2.rect):
+            		salir=True  		
+            	
+            if event.type == pygame.MOUSEBUTTONUP:
+            	colorF=blanco
+        
+        reloj1.tick(20)#operacion para que todo corra a 20fps
+        cursor1.update()
+        pantalla.fill(colorF)
+        pantalla.blit(boton1.imagen1, boton1.rect)
+        pantalla.blit(boton2.imagen1, boton2.rect)
+        
+        pygame.display.update() #actualizo el display
+        
+    pygame.quit()
+    
+principal()
