d68ea2862c137906885f96995662b7ce0d788a78
jupmorenor@gmail.com <jupmorenor@gmail.com@870897f8-f528-03ff-81b3-6bcd53e681a0>
2012-07-01 22:48:18 +0000

    Implementacion completa de las funcionalidades del menú ya existentes, falta ubicar mejor los botones de acuerdo con la imagen de fondo

--- branches/menu.py
+++ branches/menu.py
@@ -1,73 +1,93 @@
-
-
-
+# -*- coding: UTF-8 -*-
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+'''
+Created on 30/06/2012
+@author: Juanpa y Yami
+'''
 import pygame
-import interfazNivel_1
+import sys
+from generales import Generales
+from nivel_1 import Nivel1
+from objetos import Cursor, Boton
 
-
- # importo el modulo
-
-class Cursor(pygame.Rect):
+class UDTanks():
+	
 	def __init__(self):
-		pygame.Rect.__init__(self,0,0,1,1)
-	def update(self):
-		self.left,self.top=pygame.mouse.get_pos()
-class Boton(pygame.sprite.Sprite):
-	def __init__(self,imagen, x, y):
-		self.imagen1=imagen
-		self.rect= self.imagen1.get_rect()
-		self.rect.left,self.rect.top=(x,y)
- class menu():
-  #funcion main
-  def principal():
-    pygame.init() 
-       
-    pantalla=pygame.display.set_mode((500,400))
-    
-    pygame.display.set_caption("Juego") 
-    #creo un reloj para controlar los fps
-    reloj1=pygame.time.Clock()
-    cursor1=Cursor()
-    
-    boton1= Boton(pygame.image.load("boton1.png"),200,100)#boton Interfaz nivel 1
-    boton2= Boton(pygame.image.load("boton2.png"),200,200)#boton Cargar jugador Guardado
-    boton3= Boton(pygame.image.load("boton3.png"),200,200)#boton Salir
-    boton4= Boton(pygame.image.load("boton4.png"),200,200)#boton Ver Creditos
-    
-    blanco=(255,255,255) 
-    colorF=blanco
-    
-    salir=False
-    #LOOP PRINCIPAL
-    while salir!=True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                salir=True
-            if event.type== pygame.MOUSEBUTTONDOWN:
-            	if cursor1.colliderect(boton1.rect):
-            		import interfazNivel_1
-            		nivel1=interfazNivel_1.Nivel1()
-            		nivel1.mainNivel1()
-            		salir=True
-              if cursor1.colliderect(boton2.rect):
-                #aca toca hacer la sobre carga del metodo
-                import interfazNivel_1
-            		nivel1=interfazNivel_1.Nivel1()
-            		nivel1.mainNivel1()
-            		salir=True
-            	if cursor1.colliderect(boton3.rect):
-            		salir=True
-              if cursor1.colliderect(boton4.rect):
-                #ver creditos
-        
-        reloj1.tick(20)#operacion para que todo corra a 20fps
-        cursor1.update()
-        pantalla.fill(colorF)
-        pantalla.blit(boton1.imagen1, boton1.rect)
-        pantalla.blit(boton2.imagen1, boton2.rect)
-        
-        pygame.display.update() #actualizo el display
-        
-    pygame.quit()
-    
-
+		pygame.init()
+		self.funciones = Generales()
+		self.ventana = pygame.display.set_mode(self.funciones.VENTANA)
+		self.imagen_fondo = self.funciones.cargar_imagen("imagenes/menu.jpg")
+		self.musica_fondo = "sonido/TWIN CRESCENT.ogg"
+		self.empezar = Boton(self.funciones.cargar_imagen("imagenes/boton1.png"),200,100)#boton para nivel 1
+		self.continuar = Boton(self.funciones.cargar_imagen("imagenes/boton2.png"),200,150)#boton Cargar jugador Guardado
+		self.puntajes = Boton(self.funciones.cargar_imagen("imagenes/boton3.png"),200,200)#boton ver puntajes
+		self.creditos = Boton(self.funciones.cargar_imagen("imagenes/boton4.png"),200,250)#boton ver creditos
+		self.salir = Boton(self.funciones.cargar_imagen("imagenes/boton5.png"),200,300)#boton salir 
+		self.cursor = Cursor()
+		
+	def ejecutar(self):
+		pygame.init()
+		pygame.display.set_caption("UDTanks") 
+		self.funciones.cargar_musica(self.musica_fondo)
+		pygame.mixer.music.play(-1)
+		
+		
+		while True:
+			
+			for evento in pygame.event.get():
+				if evento.type == pygame.QUIT or evento.type == pygame.K_ESCAPE:
+					pygame.mixer.music.stop()
+					pygame.quit()
+					sys.exit()
+				elif evento.type == pygame.MOUSEBUTTONDOWN:
+					if self.cursor.colliderect(self.empezar):
+						pygame.mixer.music.stop()
+						nivelEnEjecucion = Nivel1(self.ventana)
+						nivelEnEjecucion.mainNivel1() # ejecuta el nivel 1
+						self.funciones.cargar_musica(self.musica_fondo)
+						pygame.mixer.music.play(-1)
+					elif self.cursor.colliderect(self.continuar):
+						pygame.mixer.music.stop()
+						nivelEnEjecucion = Nivel1(self.ventana)
+						nivelEnEjecucion.cargarDatos("Tanque 1")# carga los datos guardados
+						nivelEnEjecucion.mainNivel1()
+						self.funciones.cargar_musica(self.musica_fondo)
+						pygame.mixer.music.play(-1)
+					elif self.cursor.colliderect(self.puntajes):
+						#pygame.mixer.music.stop()
+						pass#por implementar
+						#self.funciones.cargar_musica(self.musica_fondo)
+						#pygame.mixer.music.play(-1)
+					elif self.cursor.colliderect(self.creditos):
+						pygame.mixer.music.stop()
+						from creditos import Creditos
+						nivelEnEjecucion2 = Creditos(self.ventana)
+						nivelEnEjecucion2.mainCreditos() # abre la ventana de creditos
+						self.funciones.cargar_musica(self.musica_fondo)
+						pygame.mixer.music.play(-1)
+					elif self.cursor.colliderect(self.salir):
+						pygame.mixer.music.stop()
+						pygame.quit()
+						sys.exit()
+			
+			self.cursor.actualizar()
+			self.ventana.blit(self.imagen_fondo, (0,0))
+			self.ventana.blit(self.empezar.imagen, self.empezar.rect)
+			self.ventana.blit(self.continuar.imagen, self.continuar.rect)
+			self.ventana.blit(self.puntajes.imagen, self.puntajes.rect)
+			self.ventana.blit(self.creditos.imagen, self.creditos.rect)
+			self.ventana.blit(self.salir.imagen, self.salir.rect)
+			pygame.display.update()
+		return 0
+	
+if __name__ == '__main__':
+	juego = UDTanks()
+	juego.ejecutar()
