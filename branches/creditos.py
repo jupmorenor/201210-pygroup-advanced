92c1fb8a8f40e28fcf8a09569908f4ffaa4b51d7
jupmorenor@gmail.com <jupmorenor@gmail.com@870897f8-f528-03ff-81b3-6bcd53e681a0>
2012-07-06 03:07:09 +0000

    Se completa el código de alejandro XD

--- branches/creditos.py
+++ branches/creditos.py
@@ -27,7 +27,7 @@ class Creditos():
 		self.linea2_img, self.linea2_rect = self.funciones.texto("EL GRUPO DE TRABAJO PYGROUP", 400, 232, self.color_texto)
 		self.linea3_img, self.linea3_rect = self.funciones.texto("POR:", 400, 248, self.color_texto)
 		self.linea4_img, self.linea4_rect = self.funciones.texto("JUAN PABLO MORENO RICO     2011102059", 400, 264, self.color_texto)
-		self.linea5_img, self.linea5_rect = self.funciones.texto("ALEJANDRO DUARTE     20101020***", 400, 280, self.color_texto)
+		self.linea5_img, self.linea5_rect = self.funciones.texto("ALEJANDRO DUARTE     20092020120", 400, 280, self.color_texto)
 		self.linea6_img, self.linea6_rect = self.funciones.texto("Musica por THE CROW'S CLAW", 400, 312, self.color_texto)
 		self.linea7_img, self.linea7_rect = self.funciones.texto("Imagenes tomadas de Internet de varios sitios", 400, 328, self.color_texto)
 		self.linea8_img, self.linea8_rect = self.funciones.texto("AGRADECEMOS EL INCENTIVAR ESTAS ACTIVIDADES", 400, 360, self.color_texto)
